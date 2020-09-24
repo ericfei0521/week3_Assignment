ebd013a786995d57ed8fc491158df3c7f6e23f27
ericfei306 <eric0521bigjohn@gmail.com>
2020-09-25 01:07:01 +0800

    assignment 01 eddited

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const app = express();
+const path = require('path');
+// assignment01
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/views'+'/index.html'));
+});
+
+
+app.listen(3000);
+console.log('Running at Port 3000');
