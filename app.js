a2b4631245c59cd2928a41446a553571ec99eb56
ericfei306 <eric0521bigjohn@gmail.com>
2020-09-25 13:29:53 +0800

    assignment02 updated

--- app.js
+++ app.js
@@ -2,9 +2,36 @@ const express = require('express');
 const app = express();
 const path = require('path');
 // assignment01
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/views'+'/index.html'));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views' + '/index.html'));
 });
+
+// assignment02
+app.get('/getData', function (req, res) {
+    const {number} = req.query;
+    if(isNaN(number)===false && number>0){
+        const returnNum = sumNum(number);
+        res.send(`<h1>Result is ${returnNum} </h1>`)
+        console.log(number)
+    }else if(!number){
+        res.send(`<h1>Lack of Paramete</h1>`)
+    }
+    else{
+        res.send(`<h1>Wrong Parameter</h1>`)
+    }
+});
+//加總函式
+function sumNum(number){
+    let sum = 0;
+    for(let i=0;i<= number;i++){
+        sum += i;
+    }
+    return sum;
+}
+// assignment03
+
+
+
 
 
 app.listen(3000);
